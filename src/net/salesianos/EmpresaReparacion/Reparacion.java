35bc9a6495b2b61260119fd1d6c1583dd5123349
usr3498-svg <usr3498@salesianos-lacuesta.net>
2026-04-30 13:50:52 +0100

    Update Reparacion.java
    Creo el contenido del archivo Reparacion.java con:
    -los atributos
    -el constructor
    -métodos de acceso
    -toString
    -equals

--- src/net/salesianos/EmpresaReparacion/Reparacion.java
+++ src/net/salesianos/EmpresaReparacion/Reparacion.java
@@ -1,5 +1,67 @@
 package net.salesianos.EmpresaReparacion;
 
 public class Reparacion {
-    
+
+    private int codigo;
+    private String cliente;
+    private String descripcion;
+    private boolean urgente;
+
+    public Reparacion(int codigo, String cliente, String descripcion) {
+        this.codigo = codigo;
+        this.cliente = cliente;
+        this.descripcion = descripcion;
+        this.urgente = false;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean getUrgente() {
+        return urgente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setUrgente(boolean urgente) {
+        this.urgente = urgente;
+    }
+
+    @Override
+    public String toString() {
+        return "Reparacion{" +
+                "codigo=" + codigo +
+                ", cliente='" + cliente + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", urgente=" + urgente +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Reparacion) {
+            return this.codigo == ((Reparacion) obj).codigo;
+        }
+        return false;
+    }
+
 }
